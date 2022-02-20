8ec646b29debc0634735183140544f17053ded3b
Ondřej Čížek <ondrejcizek@Ondrejs-MacBook-Pro.local>
2022-02-20 18:54:57 +0100

    OBJECTS - Dynamic Property Access & Setting Properties Dynamically

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,10 +3,13 @@ const movieList = document.getElementById('movie-list');
 movieList.style['background-color'] = 'red';
 movieList.style.display = 'block';
 
+const userChosenKeyName = 'level';
+
 let person = {
   'first-name': 'Ondrej',
   age: 25,
   hobbies: ['Gaming', 'Running'],
+  [userChosenKeyName]: '...',
   greet: function () {
     alert('Hi there!');
   },
@@ -18,6 +21,10 @@ delete person.age;
 // person.age = undefined;
 person.isAdmin = true;
 
+const keyName = 'first-name';
+
+console.log(person[userChosenKeyName]);
+console.log(person[keyName]);
 console.log(person['first-name']);
 console.log(person[1.5]);
 console.log(person);
