ec53be83d5f480781ea6dfaa89fcb3177f0cac49
Ondřej Čížek <ondrejcizek@Ondrejs-MacBook-Pro.local>
2022-02-20 18:38:34 +0100

    OBJECTS - Special Key Names & Square Bracket Property Access

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,5 +1,10 @@
-const person = {
-  name: 'Ondrej',
+const movieList = document.getElementById('movie-list');
+
+movieList.style['background-color'] = 'red';
+movieList.style.display = 'block';
+
+let person = {
+  'first-name': 'Ondrej',
   age: 25,
   hobbies: ['Gaming', 'Running'],
   greet: function () {
@@ -12,4 +17,4 @@ delete person.age;
 // person.age = undefined;
 person.isAdmin = true;
 
-console.log(person);
+console.log(person['first-name']);
