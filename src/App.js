008ab49cddd9523987bc880c9f9e9c3580aa3a3d
Ondřej Čížek <ondrejcizek@Ondrejs-MBP.home>
2022-03-01 20:03:32 +0100

    React - Practice Module - FORM VALIDATION - 100%

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-import AddUser from './components/Users/AddUser'
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
-    return (
-        <div>
-            <AddUser />
-        </div>
-    )
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
 }
 
-export default App
+export default App;
